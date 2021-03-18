32450553f48de21af8c1d59fadeffe8e96d1e4ec
KacperGielda <gkapi9@interia.eu>
2021-03-18 15:05:01 +0100

    feature/dataList store setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,10 +6,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users:[],
+  },
+  getters:{
+    users(state){
+      return state.users;
+    }
   },
   mutations: {
   },
   actions: {
+    async fetchUsers({state}){
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+      if (!data) return;
+      state.users = data;
+    }
   },
   modules: {
   }
